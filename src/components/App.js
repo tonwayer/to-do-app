9b3222cba2c7c84b0ea0afbb3888ae3c5a32fadd
tonwayer <hugtrayvon@gmail.com>
2021-02-02 13:32:29 -0500

    init commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Nav from "./Nav";
+import ToDoBox from "./ToDoBox";
+import Greet from "./Greet";
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Greet />
+      <ToDoBox />
+    </div>
+  );
+}
+
+export default App;
